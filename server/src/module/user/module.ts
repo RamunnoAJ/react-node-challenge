25d8e6096c7e4da211067e9b9440a1c98d9a634b
RamunnoAJ <ramunnoagustin@gmail.com>
2024-02-16 17:20:48 -0300

    feat: user module

---
+++ server/src/module/user/module.ts
@@ -0,0 +1,11 @@
+import { Application } from 'express'
+import { UserController } from './controller/userController'
+import { UserRepository } from './repository/json/userRepository'
+import { UserService } from './service/userService'
+
+function init(app: Application, container: any) {
+  const controller = container.get('UserController')
+  controller.configureRoutes(app)
+}
+
+export { init, UserController, UserService, UserRepository }

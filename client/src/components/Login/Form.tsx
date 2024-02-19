2d81cfa3f62e2c59463bca6e49391b2b17c3e816
RamunnoAJ <ramunnoagustin@gmail.com>
2024-02-19 17:13:00 -0300

    feat: login functionallity

--- client/src/components/Login/Form.tsx
+++ client/src/components/Login/Form.tsx
@@ -2,22 +2,37 @@
 import React from 'react'
 import Input from './Input'
 import Form from '../Form'
+import { signIn } from 'next-auth/react'
+import { toast } from 'react-toastify'
 
 export default function LoginForm() {
   const [isLoading, setIsLoading] = React.useState(false)
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     setIsLoading(true)
+
+    await toast.promise(
+      signIn('credentials', {
+        username: e.currentTarget.username.value,
+        password: e.currentTarget.password.value,
+        callbackUrl: '/',
+      }),
+      {
+        pending: 'Iniciando sesión',
+        success: 'Sesión iniciada',
+        error: 'Error al iniciar sesión',
+      },
+    )
   }
 
   return (
     <Form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
       <Input
-        label='Tu email'
-        type='email'
-        name='email'
+        label='Tu nombre de usuario'
+        type='text'
+        name='username'
         isRequired={true}
-        placeholder='name@company.com'
+        placeholder='Nombre de usuario'
       />
       <Input
         label='Tu contraseña'

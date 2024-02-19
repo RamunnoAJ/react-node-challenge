3b31fbdb624c28feac479804067a1071b0b74df8
RamunnoAJ <ramunnoagustin@gmail.com>
2024-02-19 15:11:47 -0300

    feat: card endpoint to get the cards from one user

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 
 import { configureDI } from './config/di'
 import { init as initUserModule } from './module/user/module'
+import { init as initCardModule } from './module/card/module'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,9 +18,13 @@ const container: any = configureDI()
 app.use(container.get('session'))
 
 initUserModule(app, container)
+initCardModule(app, container)
 
 const userController = container.get('UserController')
-app.get('/', userController.getAll.bind(userController))
-app.get('/:id', userController.getByID.bind(userController))
+app.get('/users/', userController.getAll.bind(userController))
+app.get('/users/:id', userController.getByID.bind(userController))
+
+const cardController = container.get('CardController')
+app.get('/cards/:id', cardController.getByUser.bind(cardController))
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
